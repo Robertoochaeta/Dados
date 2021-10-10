3e340c668241479cd6ac54add72b1c97ca545f51
Roberto <mario24roberto@gmail.com>
2021-10-10 11:50:54 -0600

    agregando la funcion Math.round()

--- Dados/src/app/app.component.ts
+++ Dados/src/app/app.component.ts
@@ -9,4 +9,12 @@ export class AppComponent {
   title = 'Dados';
   DadoIzquierda ="../assets/img/dice1.png";
   DadoDerecha="../assets/img/dice4.png";
+  numero1:number=0;
+  numero2:number=0;
+  tirarDados():void{
+    this.numero1= Math.round(Math.random() *6);
+    this.numero2=Math.round(Math.random() *6 );
+    console.log(this.numero1);
+ 
+  }
 }
