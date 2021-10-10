d710c2db6bf7900934f608a4875ff318b41a9ba1
Roberto <mario24roberto@gmail.com>
2021-10-10 12:05:56 -0600

    Finalizando EL proyecto

--- Dados/src/app/app.component.ts
+++ Dados/src/app/app.component.ts
@@ -9,12 +9,15 @@ export class AppComponent {
   title = 'Dados';
   DadoIzquierda ="../assets/img/dice1.png";
   DadoDerecha="../assets/img/dice4.png";
-  numero1:number=0;
-  numero2:number=0;
+  numero1:number=1;
+  numero2:number=2;
   tirarDados():void{
-    this.numero1= Math.round(Math.random() *6);
-    this.numero2=Math.round(Math.random() *6 );
-    console.log(this.numero1);
+    this.numero1= Math.round(Math.random() *5+1);
+    this.numero2=Math.round(Math.random() *5 +1 );
+    // console.log(this.numero1);
+    // console.log(this.numero2)
+    this.DadoIzquierda="../assets/img/dice" + this.numero1 + '.png';
+    this.DadoDerecha ="../assets/img/dice" + this.numero2 + '.png';
  
   }
 }
